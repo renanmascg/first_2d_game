c6735f8e8fc9161a998aa39f901a9158782a6552
renan mascarenhas goncalves <renanmascg@gmail.com>
2019-05-24 20:43:35 -0300

    Troca as atitudes do personagem para a classe Player em OOP.

--- jump_logic.py
+++ jump_logic.py
@@ -17,17 +17,6 @@ ALTURA = 480
 # define a altura maxima do pulo
 ALTURA_MAXIMA_PULO = 8
 
-# VELOCIDADE DO JOGO
-VELOCIDADE_JOGO = 5
-
-# TAMANHO_PERSONAGEM
-TAMANHO_PERSONAGEM = 10
-
-# UTILIZADAS NO MOVIMENTO
-direita = False
-esquerda = False
-transicao_de_imagem = 0
-
 imgs_andar_direita = [pygame.image.load('GameImages/R{}.png'.format(i)) for i in range(1, 10)]
 imgs_andar_esquerda = [pygame.image.load('GameImages/L{}.png'.format(i)) for i in range(1, 10)]
 img_personagem = pygame.image.load('GameImages/standing.png')
@@ -38,84 +27,94 @@ try:
 except:
     print("Algo inesperado ocorreu ao iniciar o pygame.")
 
-# inicializa a posicao do personagem
-pos_x = 64
-pos_y = 280
-
-is_pulando = False
 sair_jogo = False
-
-altura_pulo_atual = ALTURA_MAXIMA_PULO
 
 tela = pygame.display.set_mode([LARGURA, ALTURA])
 pygame.display.set_caption("Jump Logic")
 
 
-def redesenhar_tela(pos_x, pos_y):
-    global transicao_de_imagem
+class Player:
+    def __init__(self, pos_x, pos_y, largura, altura):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.largura = largura
+        self.altura = altura
+        self.velocidade = 5
+        self.is_pulando = False
+        self.direita = False
+        self.esquerda = False
+        self.altura_pulo_atual = ALTURA_MAXIMA_PULO
+        self.transicao_de_imagem = 0
 
+    def movimentar_personagem(self, key_pressed):
+        if key_pressed[pygame.K_RIGHT]:
+            self.pos_x += player.velocidade
+            self.direita = True
+            self.esquerda = False
+
+        elif key_pressed[pygame.K_LEFT]:
+            self.pos_x -= player.velocidade
+            self.direita = False
+            self.esquerda = True
+        else:
+            self.direita = False
+            self.esquerda = False
+            self.transicao_de_imagem = 0
+
+        if key_pressed[pygame.K_SPACE]:
+            self.is_pulando = True
+
+        if self.is_pulando:
+            if player.altura_pulo_atual >= -ALTURA_MAXIMA_PULO:
+                fl_caindo = 1
+
+                if self.altura_pulo_atual < 0:  # inversao para o personagem cair
+                    fl_caindo = -1
+
+                self.pos_y -= (self.altura_pulo_atual ** 2) / 2 * fl_caindo
+                self.altura_pulo_atual -= 1
+
+            else:
+                self.altura_pulo_atual = ALTURA_MAXIMA_PULO
+                self.is_pulando = False
+
+    def desenhar_personagem(self, tela):
+        if self.transicao_de_imagem + 1 == 27:
+            self.transicao_de_imagem = 0
+
+        if self.direita:
+            tela.blit(imgs_andar_direita[self.transicao_de_imagem // 3], (self.pos_x, self.pos_y))
+            self.transicao_de_imagem += 1
+
+        elif self.esquerda:
+            tela.blit(imgs_andar_esquerda[self.transicao_de_imagem // 3], (self.pos_x, self.pos_y))
+            self.transicao_de_imagem += 1
+
+        else:
+            tela.blit(img_personagem, (self.pos_x, self.pos_y))
+            self.transicao_de_imagem = 0
+
+
+def redesenhar_tela():
     controle_FPS.tick(27)
     tela.blit(background, (0, 0))
 
-    # CONFIGURA AS IMAGENS A SEREM CARREGADAS AO ANDAR EM CADA DIREÇÃO
-    # faz voltar a lista para o inicio
-    print(transicao_de_imagem)
-    if transicao_de_imagem + 1 == 27:
-        transicao_de_imagem = 0
-
-    if direita:
-        tela.blit(imgs_andar_direita[transicao_de_imagem // 3], (pos_x, pos_y))
-        transicao_de_imagem += 1
-
-    elif esquerda:
-        tela.blit(imgs_andar_esquerda[transicao_de_imagem // 3], (pos_x, pos_y))
-        transicao_de_imagem += 1
-
-    else:
-        tela.blit(img_personagem, (pos_x, pos_y))
-        transicao_de_imagem = 0
+    player.desenhar_personagem(tela)
 
     pygame.display.update()
 
 
+player = Player(300, 410, 64, 64)
 while not sair_jogo:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sair_jogo = True
 
-    key_press = pygame.key.get_pressed()
+    key_pressed = pygame.key.get_pressed()
 
-    if key_press[pygame.K_RIGHT]:
-        pos_x += VELOCIDADE_JOGO
-        direita = True
-        esquerda = False
+    player.movimentar_personagem(key_pressed)
 
-    elif key_press[pygame.K_LEFT]:
-        pos_x -= VELOCIDADE_JOGO
-        direita = False
-        esquerda = True
-    else:
-        direita = False
-        esquerda = False
-        transicao_de_imagem = 0
-
-    if key_press[pygame.K_SPACE]:
-        is_pulando = True
-
-    if is_pulando:
-        if altura_pulo_atual >= -ALTURA_MAXIMA_PULO:
-            fl_caindo = 1
-
-            if altura_pulo_atual < 0:  # inversao para o personagem cair
-                fl_caindo = -1
-
-            pos_y -= (altura_pulo_atual ** 2) / 2 * fl_caindo
-            altura_pulo_atual -= 1
-        else:
-            altura_pulo_atual = ALTURA_MAXIMA_PULO
-            is_pulando = False
-
-    redesenhar_tela(pos_x, pos_y)
+    redesenhar_tela()
 
 pygame.quit()
